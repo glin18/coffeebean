cc745b0de356f0d966a5779973dfe4f5db579064
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-01-14 21:14:53 +0800

    Add path to admin add products page

--- src/main/java/com/coffeebean/coffee_project/controllers/AdminController.java
+++ src/main/java/com/coffeebean/coffee_project/controllers/AdminController.java
@@ -2,12 +2,25 @@ package com.coffeebean.coffee_project.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.coffeebean.coffee_project.entity.Product;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 	
-	@GetMapping("/admin")
+	@GetMapping
 	public String adminPage() {
 		return "admin";
+	}
+	
+	@GetMapping("/products/add")
+	public ModelAndView adminAddProducts() {
+		ModelAndView mav = new ModelAndView("admin-add-products");
+		Product product = new Product();
+		mav.addObject("product", product);
+		return mav;
 	}
 }
