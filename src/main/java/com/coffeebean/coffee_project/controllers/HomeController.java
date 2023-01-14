94f41a0dd2a0ab4be9ef0818f7a9dbee07510120
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-01-14 14:48:05 +0800

    Created controller and mappings to home page

---
+++ src/main/java/com/coffeebean/coffee_project/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.coffeebean.coffee_project.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping({"/", "/home"})
+	public String home() {
+		return "home";
+	}
+}
