7a8d2d6d249a4fc65c9642173413e4a4dd63b346
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-01-14 22:29:08 +0800

    Create product in database during form submission, then redirect back to admins page

--- src/main/java/com/coffeebean/coffee_project/controllers/AdminController.java
+++ src/main/java/com/coffeebean/coffee_project/controllers/AdminController.java
@@ -1,15 +1,26 @@
 package com.coffeebean.coffee_project.controllers;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.coffeebean.coffee_project.entity.Product;
+import com.coffeebean.coffee_project.repository.ProductRepository;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	
+	@Autowired
+	ProductRepository productRepository;
 	
 	@GetMapping
 	public String adminPage() {
@@ -17,10 +28,18 @@ public class AdminController {
 	}
 	
 	@GetMapping("/products/add")
-	public ModelAndView adminAddProducts() {
+	public ModelAndView addProductForm() {
 		ModelAndView mav = new ModelAndView("admin-add-products");
 		Product newProduct = new Product();
 		mav.addObject("newProduct", newProduct);
 		return mav;
 	}
+	
+	@PostMapping("/products/add")
+	public void addProduct(@ModelAttribute Product newProduct, HttpServletResponse response) throws IOException {
+		productRepository.save(newProduct);
+		response.sendRedirect("/admin");
+	}
+	
+	
 }
