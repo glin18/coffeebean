41c50b4d144f4218573835aaf959d68497640bea
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-01-14 15:41:53 +0800

    Add get mapping to admin page

---
+++ src/main/java/com/coffeebean/coffee_project/controllers/AdminController.java
@@ -0,0 +1,13 @@
+package com.coffeebean.coffee_project.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+	
+	@GetMapping("/admin")
+	public String adminPage() {
+		return "admin";
+	}
+}
---
+++ src/main/resources/templates/admin.html
@@ -0,0 +1,1 @@
+<h1>Admin Control Panel</h1>
