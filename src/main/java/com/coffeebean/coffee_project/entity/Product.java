aa2d1a26eaff9848c46d515fa83ec54e17ba80d3
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-01-14 20:20:06 +0800

    Add Product Entity

---
+++ src/main/java/com/coffeebean/coffee_project/entity/Product.java
@@ -0,0 +1,29 @@
+package com.coffeebean.coffee_project.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private int price;
+	private String description;
+	private int stock;
+	private String image;
+	
+	
+}
