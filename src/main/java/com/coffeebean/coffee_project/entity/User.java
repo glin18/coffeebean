092eb38e70d305c68df0f9002ddc8d83e8177734
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-01-15 16:38:57 +0800

    Add a few fields to User Class

--- src/main/java/com/coffeebean/coffee_project/entity/User.java
+++ src/main/java/com/coffeebean/coffee_project/entity/User.java
@@ -1,5 +1,12 @@
 package com.coffeebean.coffee_project.entity;
 
+
 public class User {
+	
+	private Long id;
+	private String email;
+	private String username;
+	private String password;
+	private String phoneNumber;
 	
 }
