b67f94031f6d9845b396cb4dc663f0a282b1b24d
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-01-15 10:25:53 +0800

    Display products using cards on the show beans page

--- src/main/java/com/coffeebean/coffee_project/controllers/ShoppingController.java
+++ src/main/java/com/coffeebean/coffee_project/controllers/ShoppingController.java
@@ -1,15 +1,28 @@
 package com.coffeebean.coffee_project.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.coffeebean.coffee_project.entity.Product;
+import com.coffeebean.coffee_project.repository.ProductRepository;
 
 @Controller
 @RequestMapping("/products")
 public class ShoppingController {
 	
+	@Autowired
+	ProductRepository productRepository;
+
 	@GetMapping("/beans")
-	public String showBeans() {
-		return "show-beans";
+	public ModelAndView showBeans() {
+		ModelAndView mav = new ModelAndView("show-beans");
+		List<Product> allProducts = productRepository.findAll();
+		mav.addObject("allProducts", allProducts);
+		return mav;
 	}
 }
