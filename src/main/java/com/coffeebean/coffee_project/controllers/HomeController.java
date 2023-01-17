ea1301a660ed30db6c5aff747576cb819dc51735
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-01-17 13:55:41 +0800

    Display username on home page when logged in

--- src/main/java/com/coffeebean/coffee_project/controllers/HomeController.java
+++ src/main/java/com/coffeebean/coffee_project/controllers/HomeController.java
@@ -1,13 +1,28 @@
 package com.coffeebean.coffee_project.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.coffeebean.coffee_project.entity.User;
+import com.coffeebean.coffee_project.security.SecurityUtil;
+import com.coffeebean.coffee_project.service.UserService;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private UserService userService;
 
 	@GetMapping({"/", "/home"})
-	public String home() {
+	public String home(Model model) {
+		String email = SecurityUtil.getSessionUser();
+		System.out.println(email);
+		if(email != null) {
+			User user = userService.findByEmail(email);
+			model.addAttribute("user", user);
+		}
 		return "home";
 	}
 }
--- src/main/java/com/coffeebean/coffee_project/security/SecurityUtil.java
+++ src/main/java/com/coffeebean/coffee_project/security/SecurityUtil.java
@@ -3,17 +3,22 @@ package com.coffeebean.coffee_project.security;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public class SecurityUtil {
 	
 	public static String getSessionUser() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		// SecurityContextHolder is where Spring Security stores the details of who is authenticated
-		// SecurityContext is obtained from the SecurityContextHolder, and contains the authentication
-		// of the current authenticated user
-		
+		// SecurityContext is obtained from the SecurityContext 
 		if(!(authentication instanceof AnonymousAuthenticationToken)) {
-			String username = authentication.getName();
+			Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			String username;
+			if (principal instanceof UserDetails) {
+			  username = ((UserDetails)principal).getUsername();
+			} else {
+			  username = principal.toString();
+			}
 			return username;
 		}
 		return null;
