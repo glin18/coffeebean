fec6ed601d3ea25850a2d88fa5461976910c756d
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-01-18 10:45:21 +0800

    Add account info page controller and links in navbar

---
+++ src/main/java/com/coffeebean/coffee_project/controllers/AccountController.java
@@ -0,0 +1,31 @@
+package com.coffeebean.coffee_project.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.coffeebean.coffee_project.entity.User;
+import com.coffeebean.coffee_project.security.SecurityUtil;
+import com.coffeebean.coffee_project.service.UserService;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping()
+	public String getAccountInfo(Model model) {
+		String email = SecurityUtil.getSessionUser();
+		User user = userService.findByEmail(email);
+		if(user != null) {
+			model.addAttribute("user", user);
+			return "account";
+		} else {
+			return "redirect:/users/login?loginrequired";
+		}
+	}
+}
