5aa345b006659c15c9038654ee6b99d129a5d58f
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-01-22 18:15:10 +0800

    Create ChargeController

--- src/main/java/com/coffeebean/coffee_project/controllers/ChargeController.java
+++ src/main/java/com/coffeebean/coffee_project/controllers/ChargeController.java
@@ -1,5 +1,48 @@
 package com.coffeebean.coffee_project.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.coffeebean.coffee_project.entity.User;
+import com.coffeebean.coffee_project.model.ChargeRequest;
+import com.coffeebean.coffee_project.model.ChargeRequest.Currency;
+import com.coffeebean.coffee_project.security.SecurityUtil;
+import com.coffeebean.coffee_project.service.StripeService;
+import com.coffeebean.coffee_project.service.UserService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+
+@Controller
 public class ChargeController {
 
+    @Autowired
+    private StripeService paymentsService;
+    
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/charge")
+    public String charge(ChargeRequest chargeRequest, Model model)
+      throws StripeException {
+        chargeRequest.setDescription("Example charge");
+        chargeRequest.setCurrency(Currency.EUR);
+        Charge charge = paymentsService.charge(chargeRequest);
+        model.addAttribute("id", charge.getId());
+        model.addAttribute("status", charge.getStatus());
+        model.addAttribute("chargeId", charge.getId());
+        model.addAttribute("balance_transaction", charge.getBalanceTransaction());
+        String email = SecurityUtil.getSessionUser();
+		User user = userService.findByEmail(email);
+		model.addAttribute("user", user);
+        return "result";
+    }
+
+    @ExceptionHandler(StripeException.class)
+    public String handleError(Model model, StripeException ex) {
+        model.addAttribute("error", ex.getMessage());
+        return "result";
+    }
 }
